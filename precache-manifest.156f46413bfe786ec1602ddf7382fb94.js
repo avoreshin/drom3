b119dbfbfb11129500674ee7296020bddf9bdecc
avoreshin <avoreshin@gmail.com>
2022-05-11 22:41:44 +0300

    Updates

---
+++ precache-manifest.156f46413bfe786ec1602ddf7382fb94.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1991821fa624d6ad0913",
+    "url": "/drom3/static/js/main.1991821f.chunk.js"
+  },
+  {
+    "revision": "2a085df9fd242895a612",
+    "url": "/drom3/static/js/1.2a085df9.chunk.js"
+  },
+  {
+    "revision": "7300b2d3713101639713",
+    "url": "/drom3/static/js/runtime~main.7300b2d3.js"
+  },
+  {
+    "revision": "42723b0ab11b69081184ffdf313f44b3",
+    "url": "/drom3/index.html"
+  }
+];
