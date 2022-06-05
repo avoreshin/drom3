e12b13b47e2db5dc0f26b3c1424c2f0ad51a86ce
Andrey Oreshin <avoreshin@gmail.com>
2022-06-05 21:09:21 +0300

    feat(fix): add newAnn

--- src/pages/NewAnn/index.jsx
+++ src/pages/NewAnn/index.jsx
@@ -1,94 +1,98 @@
-import React from 'react';
-import PropTypes, {string} from "prop-types";
-import {Button, Container, Form, FormGroup, FormTextArea, Header, Input, Segment} from "semantic-ui-react";
-import {useForm} from "raact-hook-form";
+import React, {useState} from 'react';
+import PropTypes from "prop-types";
+import {Button, Container, Form, Header, Segment} from "semantic-ui-react";
+import {useForm} from "react-hook-form";
 
 import "./NewAnn.css"
-import {typeImplementation} from "@testing-library/user-event/dist/type/typeImplementation";
+
 const NewAnn = () => {
+
+    const { register, handleSubmit } = useForm();
+    const [data, setData] = useState("");
+
 
     let FormValues;
     FormValues = {
-        // * VIN
+        vin: ["VIN","Введите VIN"],// * VIN
         // Номер СТС
         // Госномер
-        make: string,   // * Марка
-        model: string,  // * Модель
+        make: ["Марка","Введите Марку автомобиля"],   // * Марка
+        model: ["Модель","Введите Модель"],  // * Модель
         // * Руль
-        year: string,// * Год выпуска
-        // * Кузов
+        year: ["Год", "Укажите год выпуска"],// * Год выпуска
+        transmission:["КПП", "Укажите тип коробки передач"],
+        drive_unit:["Привод","Укажите привод"],
+        engine:["Двигатель, объем","Например бензин, 1.6л"],
+        body_type:["Кузов","Например седан"],// * Кузов
         // Поколение
         // Модификация
         // Комплектация
         // * Пробег
-        // Цвет
+        color:["Цвет", "Укажите цвет"],// Цвет
         // Особые отметки
         // Описание
-        // * Цена
+        price:["Цена","Укажите цену"],// * Цена
         // * Статус
-        // * Город продажи
-        // * Телефон 1
+        city:["город продажи",""],// * Город продажи
+        mobile:["телефон","89123456789"]// * Телефон 1
         // Телефон 2
-
     }
 
-    const {register} = useForm<FormValues>();
-
-    return(
+       return(
         <Container>
             <Segment basic >
                 <Header as={'h2'}> Добавить объявление</Header>
                 <div style={{display: 'flex'}}>
                 <div style={{flex: '0 60%',textAlign: 'end'}} >
+
                     <Form
-                        widths = {'equal'}
-                    >
-                        <FormGroup inline>
-                            <label style = {{width: 150}}>Марка</label>
-                            <Input className={"input_block"} required placeholder='Введите марку автомобиля' />
-                        </FormGroup>
+                        onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}
+                        className={"input_block"} widths = {'equal'}>
 
-                        <FormGroup inline>
-                            <label style = {{width: 150}}>Модель</label>
-                            <Input className={"input_block"} required placeholder='Введите модель автомобиля' />
-                        </FormGroup>
 
-                        <FormGroup inline>
-                            <label style = {{width: 150}}>Стоимость</label>
-                            <Input className={"input_block"} required placeholder='Введите стоимость автомобиля' />
-                        </FormGroup>
+                        <label > {FormValues.vin[0]}</label>
+                            <input  {...register("vin")}  placeholder={FormValues.vin[1]} />
 
-                        <FormGroup inline>
-                            <label style = {{width: 150}}>Год</label>
-                            <Input className={"input_block"} required placeholder='Введите год автомобиля' />
-                        </FormGroup>
+                        <label>{FormValues.make[0]}</label>
+                            <input  {...register("make")}  placeholder={FormValues.make[1]} />
 
-                        <FormGroup inline>
-                            <label style = {{width: 150}}>Пробег</label>
-                            <Input className={"input_block"} required placeholder='Введите пробег автомобиля' />
-                        </FormGroup>
 
-                        <FormGroup inline>
-                            <label style = {{width: 150}}>Город</label>
-                            <Input className={"input_block"} required placeholder='Введите город' />
-                        </FormGroup>
 
-                        <FormGroup inline>
-                        <label style = {{width: 150}}>Телефон</label>
-                        <Input  className={"input_block"} required placeholder='Введите телефон' />
-                    </FormGroup>
+                        <label>{FormValues.drive_unit[0]}</label>
+                            <input  {...register("transmission")}  placeholder={FormValues.drive_unit[1]} />
 
-                        <FormGroup inline>
-                            <label style = {{width: 150}} >Описание</label>
-                            <FormTextArea  className={"input_block"} required placeholder='' />
-                        </FormGroup>
+
+                        <label>{FormValues.body_type[0]}</label>
+                            <input  {...register("engine")}  placeholder={FormValues.body_type[1]} />
+
+
+                        <label>{FormValues.color[0]}</label>
+                            <input  {...register("engine")}  placeholder={FormValues.color[1]} />
+
+
+
+
+                        {/*<InputAddForm  name = {FormValues.year[0]} placeholder={FormValues.year[1]} />*/}
+                        {/*<InputAddForm  name = {FormValues.transmission[0]} placeholder={FormValues.transmission[1]} />*/}
+                        {/*<InputAddForm  name = {FormValues.engine[0]} placeholder={FormValues.engine[1]} />*/}
+                        {/*<InputAddForm  name = {FormValues.drive_unit[0]} placeholder={FormValues.drive_unit[1]} />*/}
+                        {/*<InputAddForm  name = {FormValues.body_type[0]} placeholder={FormValues.body_type[1]} />*/}
+                        {/*<InputAddForm  name = {FormValues.color[0]} placeholder={FormValues.color[1]} />*/}
+                        {/*<InputAddForm  name = {FormValues.price[0]} placeholder={FormValues.price[1]} />*/}
+                        {/*<FormGroup inline >*/}
+                        {/*    <label style = {{width: 150}} >Описание</label>*/}
+                        {/*    <FormTextArea style={{paddingRight: "0"}} basic required placeholder='' />*/}
+                        {/*</FormGroup>*/}
+
+                        {/*<InputAddForm  name = {FormValues.mobile[0]} placeholder={FormValues.mobile[1]} />*/}
 
 
                         <Button
                             color={'red'}
-                        type={"submit"}
-                        content={"ОТПРАВИТЬ"}
+                            type={"submit"}
+                            content={"Подать оъявление и добавить фото"}
                         />
+                        <p><p>{data}</p></p>
                     </Form>
                 </div>
                 <div style={{flex: '1 140px'}}> </div>
@@ -108,7 +112,8 @@ NewAnn.defaultProps = {
 NewAnn.propTypes = {
     idName: PropTypes.number,
 }
-;
+
+
 
 export default NewAnn;
 
