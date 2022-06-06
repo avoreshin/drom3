e8e26c3abc5fb84c28dab7954474a47ff8c035de
Andrey Oreshin <avoreshin@gmail.com>
2022-06-06 11:39:28 +0300

    feat: add input

---
+++ src/components/InputAddForm/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { FormGroup, Input} from "semantic-ui-react";
+
+const InputAddForm =({name, placeholder})=>{
+
+    const input_block = {
+        width: "80%",
+        color: "#db2828",
+        padding: "0 1em 0 0"
+                
+    };
+
+    return (
+        <>
+            <FormGroup style= {{justifyContent: "end"}} inline>
+                <label style = {{ maxWidth:'20%'}}>{name}</label>
+                <Input style = {input_block} required placeholder={placeholder}/>
+            </FormGroup>
+        </>
+    )
+};
+
+export default InputAddForm;
