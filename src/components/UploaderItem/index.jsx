96a2b3187393657654b4cfdba942bd531dc7a159
Andrey Oreshin <avoreshin@gmail.com>
2022-06-20 20:22:52 +0300

    feat: add uploader

--- src/components/UploaderItem/index.jsx
+++ src/components/UploaderItem/index.jsx
@@ -1,56 +1,49 @@
-import React, { useState } from 'react';
-import ImgCrop from 'antd-img-crop';
-import { Upload } from 'antd';
-import 'antd/dist/antd.css';
-
-
+import React, { useState } from "react";
+import ImgCrop from "antd-img-crop";
+import { Upload } from "antd";
+import "antd/dist/antd.css";
 
 const getSrcFromFile = (file) => {
-    return new Promise((resolve) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file.originFileObj);
-        reader.onload = () => resolve(reader.result);
-    });
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file.originFileObj);
+    reader.onload = () => resolve(reader.result);
+  });
 };
 
 const UploaderItem = () => {
-    const [fileList, setFileList] = useState([
-        {
-            uid: '-1',
-            url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-        },
-    ]);
+  const [fileList, setFileList] = useState([]);
 
-    const onChange = ({ fileList: newFileList }) => {
-        setFileList(newFileList);
-    };
+  const onChange = ({ fileList: newFileList }) => {
+    setFileList(newFileList);
+  };
 
-    const onPreview = async (file) => {
-        const src = file.url || (await getSrcFromFile(file));
-        const imgWindow = window.open(src);
+  const onPreview = async (file) => {
+    const src = file.url || (await getSrcFromFile(file));
+    const imgWindow = window.open(src);
 
-        if (imgWindow) {
-            const image = new Image();
-            image.src = src;
-            imgWindow.document.write(image.outerHTML);
-        } else {
-            window.location.href = src;
-        }
-    };
+    if (imgWindow) {
+      const image = new Image();
+      image.src = src;
+      imgWindow.document.write(image.outerHTML);
+    } else {
+      window.location.href = src;
+    }
+  };
 
-    return (
-        <ImgCrop grid rotate>
-            <Upload
-                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-                listType="picture-card"
-                fileList={fileList}
-                onChange={onChange}
-                onPreview={onPreview}
-            >
-                {fileList.length < 15 && '+ Upload'}
-            </Upload>
-        </ImgCrop>
-    );
+  return (
+    <ImgCrop aspect={4 / 3} rotate grid>
+      <Upload
+        // action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+        listType="picture-card"
+        fileList={fileList}
+        onChange={onChange}
+        onPreview={onPreview}
+      >
+        {fileList.length < 15 && "+ Upload"}
+      </Upload>
+    </ImgCrop>
+  );
 };
 
 export default UploaderItem;
--- src/pages/NewAnnPhoto/index.jsx
+++ src/pages/NewAnnPhoto/index.jsx
@@ -1,16 +1,12 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 // import PropTypes from "prop-types";
 
-import {
-  Button,
-  Container,
-  Header,
-  Segment
-} from "semantic-ui-react";
+import { Button, Container, Header, Segment } from "semantic-ui-react";
 import { useForm } from "react-hook-form";
 
 import { useNavigate } from "react-router-dom";
 import UploaderItem from "../../components/UploaderItem";
+import DragAndDrop from "../../components/DragAndDrop"
 
 const NewAnnPhoto = () => {
   const { register, handleSubmit } = useForm();
@@ -28,16 +24,23 @@ const NewAnnPhoto = () => {
       <Segment basic>
         <Header as={"h2"}> Добавление фотографии</Header>
         <div style={{ display: "flex" }}>
-          <div style={{ flex: "0 60%", textAlign: "end" }}>
-      <UploaderItem />
+          <div
+            style={{
+              // flex: "0 60%",
 
-      <Button
+              // background: "#333",
+            }}
+          >
+            {/* <DragAndDrop/> */}
+            <UploaderItem />
+
+            <Button
               color={"red"}
               type={"submit"}
-              content={"Подать оъявление и добавить фото"}
+              content={"Предварительный просмотр"}
             />
           </div>
-          <div style={{ flex: "1 140px" }}> </div>
+          {/* <div style={{ flex: "1 140px" }}> </div> */}
         </div>
         <p>{onSubmit}</p>
       </Segment>
